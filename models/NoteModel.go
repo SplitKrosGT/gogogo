5ee66565fc5fbf7f6b23b1e506e58393bd747109
Павел <72755708+SplitKrosGT@users.noreply.github.com>
2022-05-21 14:07:57 +0500

    Add files via upload

--- models/NoteModel.go
+++ models/NoteModel.go
@@ -69,9 +69,10 @@ func UpdateNote(note Note) Note {
 }
 
 func DeleteNote(id int64) error {
+	var note Note
 	db := service.PgDataBase()
 
-	_, err := db.Model().Where("id = ?", id).Delete()
+	_, err := db.Model(&note).Where("id = ?", id).Delete()
 
 	db.Close()
 	return err
